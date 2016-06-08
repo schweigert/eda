1d3b891db6dc30145de2a924d06ec9485c755d78
Gustavo Diel <gustavodiel@hotmail.com>
2016-06-08 16:51:01 -0300

    Basico do menu :v ~ Diel

--- main.c
+++ main.c
@@ -2,8 +2,60 @@
 #include <stdlib.h>
 #include "ABB.h"
 
+void menu();
 
 int main (void){
-	
+	const char* file = "Arq.txt";
+    
+    menu();
+    
 	return 0;
+}
+
+void menu(){
+MENU:
+    printf("Selecione a opcao:\
+           \n1.\tCarregar arquivo.\
+           \n2.\tBuscar registro.\
+           \n3.\tRemover registro\
+           \n4.\tInserir registro.\
+           \n5.\tTamanho do arquivo.\
+           \n6.\tSalvar arquivo\
+           \n7.\tRecuperar registro a partir do arquivo.\
+           \n0.\tSair do programa.\n\t");
+    int opcao;
+    scanf("%d", &opcao);
+    switch (opcao) {
+        case 1:
+            
+            break;
+        case 2:
+            
+            break;
+        case 3:
+            
+            break;
+        case 4:
+            
+            break;
+        case 5:
+            
+            break;
+        case 6:
+            
+            break;
+        case 7:
+            
+            break;
+        case 0:
+            exit(0);
+            break;
+            
+        default:
+            printf("Opcao errada!\n");
+            goto MENU;
+            break;
+    }
+           
+           
 }
